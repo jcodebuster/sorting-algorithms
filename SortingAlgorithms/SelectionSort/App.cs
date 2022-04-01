cd9f1713785f03b0f042049ac2f00fed37b70b76
Arsenii Merkulov <arsenii.vm@gmail.com>
2022-04-01 19:26:30 +0300

    selection sort implementation

---
+++ SortingAlgorithms/SelectionSort/App.cs
@@ -0,0 +1,12 @@
+﻿namespace SelectionSort;
+
+public class App
+{
+    public void Start()
+    {
+        var sort = new SelectionSort();
+        var array = new[] {7, 5, 3, 5, 9, 51, 5};
+        sort.Sort(ref array);
+        Console.WriteLine(string.Join(" ", array));
+    }
+}
---
+++ SortingAlgorithms/SelectionSort/SelectionSort.cs
@@ -0,0 +1,33 @@
+﻿
+namespace SelectionSort;
+
+public class SelectionSort
+{
+    public void Sort(ref int[] items)
+    {
+        int sortedRange = 0;
+        while (sortedRange < items.Length)
+        {
+            var index = SmallestIndexFromRest(items, sortedRange);
+            (items[sortedRange], items[index]) = (items[index], items[sortedRange]);
+            sortedRange++;
+        }
+    }
+
+    private int SmallestIndexFromRest(int[] items, int sortedRange)
+    {
+        int current = items[sortedRange];
+        int currentIndex = sortedRange;
+
+        for (int i = currentIndex + 1; i < items.Length; i++)
+        {
+            if (items[i] < current)
+            {
+                current = items[i];
+                currentIndex = i;
+            }
+        }
+
+        return currentIndex;
+    }
+}

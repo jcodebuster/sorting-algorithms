3aab333de08c82a380c64b2bbf293e5d6345b2f5
Arsenii Merkulov <arsenii.vm@gmail.com>
2022-04-02 00:00:51 +0300

    insertion sort implementation

---
+++ SortingAlgorithms/InsertionSort/App.cs
@@ -0,0 +1,13 @@
+﻿namespace InsertionSort;
+
+public class App
+{
+    public void Run()
+    {
+        var array = new[] {2, 3, 5, 4, 8, 3, 2, 1, 5, 6};
+        var sort = new InsertionSort();
+        sort.Sort(array);
+        
+        Console.WriteLine(String.Join(" ", array));
+    }
+}
---
+++ SortingAlgorithms/InsertionSort/InsertionSort.cs
@@ -0,0 +1,45 @@
+﻿namespace InsertionSort;
+
+public class InsertionSort
+{
+    public void Sort(int[] items)
+    {
+        int sortedRangeEnd = 1;
+        while (sortedRangeEnd < items.Length)
+        {
+            if (items[sortedRangeEnd] < items[sortedRangeEnd - 1])
+            {
+                var insertIndex = FindInsertionIndex(items, items[sortedRangeEnd]);
+                InsertByIndex(items, sortedRangeEnd, insertIndex);
+            }
+
+            sortedRangeEnd++;
+        }
+    }
+
+    private int FindInsertionIndex(int[] items, int valueToInsert)
+    {
+        for (int i = 0; i < items.Length; i++)
+        {
+            if (items[i] > valueToInsert)
+            {
+                return i;
+            }
+        }
+
+        throw new InvalidOperationException();
+    }
+
+    private void InsertByIndex(int[] items, int indexFrom, int indexTo)
+    {
+        var temp = items[indexTo];
+
+        items[indexTo] = items[indexFrom];
+        for (int i = indexFrom; i > indexTo; i--)
+        {
+            items[i] = items[i - 1];
+        }
+
+        items[indexTo + 1] = temp;
+    }
+}
